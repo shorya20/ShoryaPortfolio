29a5d035a8ad5c2de6afefb13478911b09e41a52
Shorya Sinha <shoryaoct20@gmail.com>
2021-04-10 01:01:16 +0400

    changed

--- webroot/miniproject/Login.php
+++ webroot/miniproject/Login.php
@@ -5,7 +5,7 @@ $dbuser = getenv("DATABASE_USER");
 $dbpwd = getenv("DATABASE_PASSWORD");
 $dbname = getenv("DATABASE_NAME");
 // Creates connection
-$conn = new mysql($dbhost, $dbuser, $dbpwd, $dbname);
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 // Checks connection
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
@@ -17,9 +17,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
             session_start();
             $username = $_POST["usname"];
             $password = $_POST["pass"];
-            $select1 = "SELECT * from USERS WHERE email = '$username' and password ='$password'";
-            $result1=mysql_query($select1);
-            if(mysql_num_rows($result1)>0){
+            $select1 = "SELECT * from USERS WHERE email = '".$username."' and password ='"$password."'";
+            $result1=mysqli_query($conn,$select1);
+            if(mysqli_num_rows($result1)>0){
                 echo "<h6> Registration successful";
                 $_SESSION['usname']=$username;
                 $_SESSION['password']=$password;
