06b340dd977067e97a63fd49f1f34b81e29aeb4b
Shorya Sinha <shoryaoct20@gmail.com>
2021-04-10 21:28:29 +0400

    changed

--- webroot/miniproject/addPost.php
+++ webroot/miniproject/addPost.php
@@ -1,86 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="reset.css">
-        <link rel="stylesheet" href="blog.css">
-    </head>
-    <body>
-        <script>
-            function click1(){
-                var title=document.getElementById("title").value;
-                var body = document.getElementById("body").value;
-                if(title!=""){
-                    var value=window.confirm("You are about to clear your message. Click OK to confirm");
-                    if(value){
-                    document.getElementById("myform").reset();
-                    }
-                }
-                else if(body!=""){ 
-                    var value=window.confirm("You are about to clear your message. Click OK to confirm");
-                    if(value){
-                        document.getElementById("myform").reset();
-                    }
-                }
-            }
-            function checkempty(){
-                var title=document.getElementById("title").value;
-                var body = document.getElementById("body").value;
-                if(title=="" & body!=""){
-                    document.getElementById("title").style.border="2pt solid red";
-                    document.getElementById("body").style.border="2pt solid green";
-                    window.alert("Please enter a title");
-                    document.getElementById("submit").addEventListener("submit",function(event){
-                        event.preventDefault();
-                    });
-                }
-                else if(body=="" && title!=""){
-                    window.alert("Please enter a body");
-                    document.getElementById("title").style.border="2pt solid green";
-                    document.getElementById("body").style.border="2pt solid red";
-                    document.getElementById("submit").addEventListener("submit",function(event){
-                        event.preventDefault();
-                    });
-                }
-                else if(title=="" & body==""){
-                    window.alert("Please enter a title and a body");
-                    document.getElementById('title').style.border="2pt solid red";
-                    document.getElementById('body').style.border="2pt solid red";
-                    document.getElementById("submit").addEventListener("submit",function(event){
-                        event.preventDefault();
-                    });
-                }
-            }
-        </script>
-        <div class="middle">
-            <article>
-                <section class="blog_box_outside">
-                    <form method="POST" action="/action_page.php" id="myform">
-                        <fieldset class="blog_box">
-                            <h1>Add Blog</h1>
-                            <section>
-                                <div class="title">
-                                    <p>
-                                        <label for="Title"></label>
-                                            <input type="text" name="title" placeholder="Title" id="title">
-                                    </p>
-                                </div>
-                                <div class="body">
-                                    <p>
-                                        <label for="Body"></label>
-                                        <textarea id="body" placeholder="Enter your text here" name="body"></textarea>
-                                    </p>
-                                </div>
-                                <div class="submit">
-                                    <button class="input" id="submit" onclick="checkempty()">Submit</button>
-                                </div>
-                                <div class="reset">
-                                    <button class="input" onclick="click1()">Reset</button>
-                                </div>
-                            </section>
-                        </fieldset>
-                    </form>
-                </section>
-            </article>
-        </div>
-    </body>
-</html>
+<?php 
+    $dbhost = getenv("MYSQL_SERVICE_HOST");
+    $dbport = getenv("MYSQL_SERVICE_PORT");
+    $dbuser = getenv("DATABASE_USER");
+    $dbpwd = getenv("DATABASE_PASSWORD");
+    $dbname = getenv("DATABASE_NAME");
+    // Creates connection
+    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+    // Checks connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $timearray=array();
+        $datearray=array();
+        $titlearray=array();
+        $bodyarray=array();
+        date_default_timezone_set('UTC');
+        $tabletime=date("h:i");
+        $timearray[]=$tabletime;
+        $date=date('d-M-Y');
+        $datearray[]=$date;
+        asort($datearray);
+        $title = $_POST['title'];
+        $titlearray[]=$title;
+        $body = $_POST['body'];
+        $bodyarray[]=$body;
+        $sql = "INSERT INTO INPUT(time,date,title,body) VALUES($tabletime,$date,$title,$body)";
+        $_SESSION['timearray']=$timearray;
+        $_SESSION['datearray']=$datearray;
+        $_SESSION['titlearray']=$titlearray;
+        $_SESSION['bodyarray']=$bodyarray;
+        header('location:viewBlog.php');
+    }    
+?>
