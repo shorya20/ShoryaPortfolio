bcfaea7430105bc6d0a528d24cab759f6a6c6907
Shorya Sinha <shoryaoct20@gmail.com>
2021-04-11 00:13:38 +0400

    changed

--- webroot/miniproject/Login.php
+++ webroot/miniproject/Login.php
@@ -11,6 +11,8 @@ if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
 if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $_SESSION['usname']=$username;
+    $_SESSION['password']=$password;
     $select1 = "SELECT * from USERS WHERE email = '".$username."' and password ='".$password."'";
     if($conn->query($select1)==TRUE){
         if (isset($_POST["usname"], $_POST["pass"])) {
@@ -19,8 +21,6 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
             $password = $_POST["pass"];
             $result1=mysqli_query($conn,$select1);
             if(mysqli_num_rows($result1)>0){
-                $_SESSION['usname']=$username;
-                $_SESSION['password']=$password;
                 header("location: addEntry.php");
             }
             else{
